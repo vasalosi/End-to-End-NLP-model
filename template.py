ee70cfc916bfb1eb0ceb501603603e4ad0f4e960
vasalosi <98632541+vasalosi@users.noreply.github.com>
2024-11-05 09:41:46 +0200

    changed the indentation

--- template.py
+++ template.py
@@ -49,8 +49,9 @@ for filepath in list_of_files:
     if (not os.path.exists(filepath)) or (os.path.getsize(filepath) == 0):
         with open(filepath, "w") as f:
             pass
-        logging.info(f"Creating empty file: {filepath}")
+            logging.info(f"Creating empty file: {filepath}")
 
 
     else:
         logging.info(f"{filename} is already exists")
+
